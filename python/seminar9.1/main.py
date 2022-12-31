f574410ac0972a21f82047eba3df938cbed0bdd0
Artem <tomilow.art@yandex.ru>
2022-12-31 19:17:43 +1200

    DZ_9.1

---
+++ python/seminar9.1/main.py
@@ -0,0 +1,11 @@
+from pyowm import OWM
+
+ 
+owm = OWM('9042f06879c292fdb0f3af4517ebfa99')
+mgr = owm.weather_manager()
+ 
+ 
+observation = mgr.weather_at_place('Moscow, RU')
+w = observation.weather
+ 
+print(w.temperature('celsius'))  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
